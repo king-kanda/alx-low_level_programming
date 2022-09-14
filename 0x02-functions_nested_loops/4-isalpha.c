d0d957d6e41eca74b899bccfdb353d72b4a6308a
king-kanda <stevenokanda@gmail.com>
2022-09-14 01:33:14 -0700

    betty solved

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,26 +1,24 @@
 #include "main.h"
 
-/** 
-*
-*main : check is letter is capital
-*
-*Description : A program to check if the letter is capital
-*
-*return : 1 on success and 0 on failure
-*/
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: c is an ascii character
+ *
+ * Return: 1 (if letter)
+ */
 
-int _isalpha(int c) {
-
-    if(c >= 'A' &&  c <= 'Z')
-    {
-        return 1 ;
-
-    }else if (c >= 'a' && c <= 'z')
-    {
-        return 1 ;
-    }else
-    {
-        return 0 ;
-    }
-
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
